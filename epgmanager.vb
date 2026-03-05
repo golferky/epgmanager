302aab64276e0bd65a1ae7a6c66b2259ff08dee7
Gary Scudder <gary_scudder@yahoo.com>
2026-03-05 14:05:20 -0500

    ghost guide.db

--- epgmanager.vb
+++ epgmanager.vb
@@ -152,7 +152,7 @@ Module Program
 
             Dim planned = scored _
             .Where(Function(x) myChannels.Contains(x.Candidate.Channel)) _
-            .Where(Function(x) x.Candidate.StartTime > DateTime.Now) _
+            '.Where(Function(x) x.Candidate.StartTime > DateTime.Now) _
             .GroupBy(Function(x) NormalizeTitle(x.Candidate.Title)) _
             .Select(Function(g) g _
                 .OrderByDescending(Function(m) TitleHelpers.GetChannelPriority(m.Candidate.Channel)) _
